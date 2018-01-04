c1e61b1a4a30c222d39df71dc68be4930eda6a00
Peter Jodeleit <jodeleit@e-spirit.com>
2018-01-04 01:13:18 +0100

    test case for issue #1040

--- java/examples/AllInOne7.java
+++ java/examples/AllInOne7.java
@@ -330,6 +330,13 @@ class Foo { // Top-level class
     }
 }
 
+static void inner_class_constructor() {
+    // https://docs.oracle.com/javase/specs/jls/se9/html/jls-15.html#jls-15.9
+    Foo foo = new Foo();
+    Foo.Bar fooBar1 = foo.new Bar();
+    Foo.Bar fooBar2 = new Foo().new Bar();
+}
+
 // Nested class
 class Foo { // Top-level class
     static class Bar { // Nested class
